1af659e9047b64a8d8147cdb0cd053efbf976c51
keith brooks <kbrook10@gmail.com>
2017-01-05 12:37:49 -0500

    Create routes for the home resume and contacts pages

--- app/app.js
+++ app/app.js
@@ -8,14 +8,38 @@ var app = express();
 //Create flexible port value depending on environment and set default to 3000....
 app.set('port', process.env.PORT || 3000);
 
-//<------------------ Create Index Route ----------------------------->
+//<------------------ Create Application Routes ---------------------->
 //<------------------------------------------------------------------->
+
+//Root Route
 app.get('/', function(req, res) {
     res.send(`
-        <h1>Welcome to Profile Page - Keith Brooks</h1>
+        <h1>Welcome to Home Page</h1>
+        <p>This page will provide a glimpse of who I am</p>
         `);
 });
 
+//Blog Route --> This does not work...Need to just create a tag and link to new tab...
+// app.get('/blog', function(req, res) {
+//     res.send(`https://medium.com/@kbrook10
+//         `);
+// });
+
+//Resume Route
+app.get('/resume', function(req, res) {
+    res.send(`
+        <h1>Welcome to Resume Page</h1>
+        <p>This page will provide details of my work experience</p>
+        `);
+});
+
+//Contact Route
+app.get('/contact', function(req, res) {
+    res.send(`
+        <h1>Welcome to Contact Page</h1>
+        <p>This page will provide how to get in touch with me</p>
+        `);
+});
 
 //<-----------------Server to Listen in on Connections---------------->
 //<------------------------------------------------------------------->
